efbe40272448f59f3353c0a0f174f11824685a9c
achyuth <achyuthspa@gmail.com>
2021-05-08 19:47:28 +0530

    PC can now read data from a website

--- Producer.java
+++ Producer.java
@@ -1,62 +1,80 @@
-package pc_with_interrupt;
-
 import java.util.Queue;
 import java.util.Random;
 import java.util.concurrent.Callable;
+import java.io.IOException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 
-public class Producer implements Callable<Integer> {
-    private Queue<Integer> queue;
-    private int maxSize;
-    public Producer(Queue<Integer> queue, int maxSize){
-        this.queue = queue;
-        this.maxSize = maxSize;
-    }
+public class Producer implements Callable<Integer>, Messenger {
+	private Queue<String> queue;
+	private int maxSize;
+	private String website;
+	private Elements links;
+	private int linksSize;
+
+	public Producer(Queue<String> queue, int maxSize, String website){
+		this.queue = queue;
+		this.maxSize = maxSize;
+		this.website = website;
+
+		Document doc = null;
+		try {
+			doc = Jsoup.connect(this.website).userAgent("mozilla/17.0").get();
+		}
+		catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+//		String title = doc.title();
+//		System.out.println(title);
+		links = doc.select("a[href]");
+		linksSize = links.size();
+		//System.out.println(sz);
+	}
 
 	@Override
 	public Integer call() throws Exception {
-		// TODO Auto-generated method stub
-
-		//long startTime = System.nanoTime();
-		
 		try {
-			
-			while (true) {//(System.nanoTime() - startTime)/1000000000.0 < 2000) {
-				
-				/*
-				 * entering the critical section now; wrap in synchronized block
+			while (true) {
+				/* entering the critical section now; wrap in synchronized block
 				 * if queue is full, producer needs to wait
-				 * else add entry to the queue
-				 */
-				
-	            synchronized (queue) {
-	                while (queue.size() == maxSize) {
-	                	
-	                	System.out .println("Producer -- " + this.toString() + " Queue is full, will wait now");
-	                    
-	                    queue.wait();
-	                	
-	                }
-	                
-	                /*
-	                 * producer generates a random number and puts this into the queue
-	                 * need to replace this with the following:
-	                 * get some data from the web-page and put it into the queue 
-	                 */
-	                
-	                Random random = new Random();
-	                int j = random.nextInt();
-	                System.out.println("Producer -- " +  this.toString() + " Producing value : " + j);
-	                
-	                queue.add(j);
-	                queue.notifyAll();
-	                
-	            }
-	
-	        }
-		}	catch(Exception e) {
-			
+				 * else add entry to the queue */
+				synchronized (queue) {
+					while (queue.size() == maxSize) {
+						System.out .println("Producer -- " + this.toString() + " Queue is full, will wait now");
+						queue.wait();
+					}
+					/* producer gets some data from the web-page and put it into the queue */
+					String str = get();
+					//System.out.println(str + " " + queue.size() + " " + maxSize);
+					if(!str.isEmpty()) {
+						System.out.println("Producer -- " +  this.toString() + " Producing value : " + str);
+						queue.add(str);
+					}
+					queue.notifyAll();
+				}
+			}
+		}
+
+		catch(Exception e) {
 			return -1;
 		}
-		
+	}
+
+	@Override
+	public String get() {
+		// System.out.print("In producer get() ");
+		Random rand = new Random();
+		int rand_int1 = rand.nextInt(this.linksSize);
+		String str = this.links.get(rand_int1).text();
+		// System.out.println(rand_int1 + " " + str);
+		return str;
+	}
+
+	@Override
+	public void put() {
+		// TODO Auto-generated method stub
+
 	}
 }
